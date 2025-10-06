ef898c2fe6841951951ff6fb692575e584c0798f
Nathan Patrzyk <nathanpatzyk11@gmail.com>
2025-10-06 16:57:17 -0300

    refactor: alterando mesagem de inscrição não disponível

---
+++ src/components/app-card.tsx
@@ -0,0 +1,44 @@
+import { CircleAlert, CircleCheck, CircleX, Info } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+
+type AppCardProps = {
+  title: string;
+  description?: string;
+  type: "info" | "warning" | "error" | "success";
+} & React.ComponentProps<typeof Card>;
+
+export function AppCard({ title, description, type, ...props }: AppCardProps) {
+  const navigate = useNavigate();
+
+  return (
+    <Card className="w-full max-w-sm gap-0" {...props}>
+      <CardHeader className="grid-rows-[1fr_auto] gap-4">
+        {type == "info" && <Info className="mx-auto size-8 text-blue-600" />}
+        {type == "warning" && (
+          <CircleAlert className="mx-auto size-8 text-yellow-400" />
+        )}
+        {type == "error" && (
+          <CircleX className="text-destructive mx-auto size-8" />
+        )}
+        {type == "success" && (
+          <CircleCheck className="mx-auto size-8 text-green-500" />
+        )}
+        <CardTitle className="text-center">{title}</CardTitle>
+        <CardDescription className="text-center">{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="text-end">
+        <Button variant="link" onClick={() => navigate(-1)}>
+          Voltar
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/EnrollmentStudentPage.tsx
+++ src/pages/EnrollmentStudentPage.tsx
@@ -1,41 +1,58 @@
 import { useParams } from "react-router-dom";
 import { useStudentPage } from "../hooks/useStudentPage";
 import { EnrollmentStudentForm } from "../components/form/EnrollmentStudentForm";
+import { AppCard } from "@/components/app-card";
+import { formatDate } from "@/utils/formatDate";
 
-const EnrollmentClosedMessage = ({ startDate, endDate }: { startDate: string; endDate: string }) => {
-    return (
-        <div>
-            {startDate} - {endDate}
-            <h2>Inscrições Encerradas</h2>
-        </div>
-    );
+const EnrollmentClosedMessage = ({
+  startDate,
+  endDate,
+}: {
+  startDate: string;
+  endDate: string;
+}) => {
+  return (
+    <AppCard
+      title="Esta edição não está recebendo inscrições no momento."
+      description={`As inscrições estarão abertas entre ${formatDate(startDate)} e ${formatDate(endDate)}.`}
+      type="error"
+    />
+  );
 };
 
 export const EnrollmentStudentPage = () => {
-    const { editionYear } = useParams<{ editionYear: string }>();
-    const { data: editionStatus, isLoading, isError, isSuccess } = useStudentPage(editionYear);
+  const { editionYear } = useParams<{ editionYear: string }>();
+  const {
+    data: editionStatus,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useStudentPage(editionYear);
 
-    if (isLoading) {
-        return <div>Carregando informações da edição...</div>;
-    }
+  if (isLoading) {
+    return <div>Carregando informações da edição...</div>;
+  }
 
-    if (isError) {
-        return <div>Ocorreu um erro ao carregar os dados da edição.</div>;
-    }
+  if (isError) {
+    return <div>Ocorreu um erro ao carregar os dados da edição.</div>;
+  }
 
-    if (isSuccess) {
-        return editionStatus.isStudentEnrollmentOpen ? (
-            <EnrollmentStudentForm
-                minimumWage={editionStatus.minimumWage}
-                editionName={editionStatus.name}
-                editionYear={editionStatus.year} />
-        ) : (
-            <EnrollmentClosedMessage
-                startDate={editionStatus.studentRegistrationStartDate}
-                endDate={editionStatus.studentRegistrationEndDate}
-            />
-        );
-    }
+  if (isSuccess) {
+    return !editionStatus.isStudentEnrollmentOpen ? (
+      <EnrollmentStudentForm
+        minimumWage={editionStatus.minimumWage}
+        editionName={editionStatus.name}
+        editionYear={editionStatus.year}
+      />
+    ) : (
+      <div className="flex w-screen items-center justify-center p-4 md:p-8">
+        <EnrollmentClosedMessage
+          startDate={editionStatus.studentRegistrationStartDate}
+          endDate={editionStatus.studentRegistrationEndDate}
+        />
+      </div>
+    );
+  }
 
-    return null;
+  return null;
 };
